d888e020673219cde5b4998fd76661b92b664ffc
lucianobragaweb <lucianobragaweb@gmail.com>
2022-02-28 22:13:01 -0300

    prepare timeline to translate

--- react-app/src/locales/en/pages/resume.js
+++ react-app/src/locales/en/pages/resume.js
@@ -29,7 +29,50 @@ const resume = {
     },
     experiences: {
         title: "Professional Experiences",
-        companies: {}
+        companies: {
+            modal: {
+                period: "AGO/2021 - NOW",
+                role: "Senior Frontend",
+                location: "São Paulo, Brasil",
+                company: "Banco Modal",
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie quam velit, non venenatis magna imperdiet vitae. Praesent laoreet tristique quam tempor lobortis.'
+            },
+            devsquad: {
+                period: "JAN/2021 - SET/2021",
+                role: "Full Stack Developer",
+                location: "Utah, EUA",
+                company: "DevSquad",
+                description: 'Acting as a full-stack developer of modern and scalable web applications for the biggest companies in the USA, using SCRUM for project management. Docker, PHP 8, Laravel 8, Vue.js, Livewire, and TailwindCSS are the main technologies used in these projects.'
+            },
+            itransparencia: {
+                period: "2017 - 2021",
+                role: "Senior Software Engeneer",
+                location: "Ceará, Brasil",
+                company: "iTransparência Sistemas",
+                description: 'As a full stack developer and partner I was responsible for developing all the company’s products, such as Web, Desktop and Mobile Applications Software. I’ve used technologies like Node, Vue.js, React Native and Electron.'
+            },
+            amagafa: {
+                period: "MAR/2016 - AGO/2016",
+                role: "Desenvolvedor Frontend",
+                location: "São Paulo, Brasil",
+                company: "Amagafa Tech",
+                description: 'Trabalhando em um time de 10 pessoas nós construímos uma aplicação de e-learning para treinamento de primeira habilitação e reciclagem de CNH.'
+            },
+            iblue: {
+                period: "2013 - 2014",
+                role: "Desenvolvedor Web",
+                location: "Ceará, Brasil",
+                company: "iBlue",
+                description: 'Trabalhando na iBlue tive a oportunidade de aprender diversas tecnologias para Web e Mobile, por se tratar de uma Startup jovem e em desenvolvimento.'
+            },
+            freelancer: {
+                period: "2010 - 2013",
+                role: "Web Developer",
+                location: "Ceará, Brasil",
+                company: "Freelancer",
+                description: 'Studied web programming since 2007 and in 2010 I started to work as a freelancer developing websites for companies in different segments and in several regions of Brazil using technologies such as HTML, CSS, Javascript, PHP and MySQL.'
+            }
+        }
     },
     footer: {
         available_in: "Available in"
