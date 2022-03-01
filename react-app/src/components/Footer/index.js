6cd789b39b0fcd771c1ecaacd06a2a55bef80e40
lucianobragaweb <lucianobragaweb@gmail.com>
2022-03-01 15:43:12 -0300

    fix

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -9,9 +9,9 @@ export default function Footer () {
                 { t("resume.footer.available_in") } <a href='https://lucianobragaweb.github.io/resume' className='text-blue-500'>lucianobragaweb.github.io/resume</a>
             </div>
             <GitHubButton href="https://github.com/lucianobragaweb" data-show-count="true" aria-label="Follow @lucianobragaweb on GitHub">@lucianobragaweb</GitHubButton>
-            <GitHubButton href="https://github.com/lucianobragaweb/resume/fork" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork lucianobragaweb/resume on GitHub">Fork</GitHubButton>
-            <GitHubButton href="https://github.com/lucianobragaweb/resume/issues" data-icon="octicon-issue-opened" data-show-count="true" aria-label="Issue lucianobragaweb/resume on GitHub">Issue</GitHubButton>
-            <GitHubButton href="https://github.com/lucianobragaweb/resume" data-icon="octicon-star" data-show-count="true" aria-label="Star lucianobragaweb/resume on GitHub">Star</GitHubButton>
+            <GitHubButton href="https://github.com/lucianobragaweb/resume/fork" data-icon="octicon-repo-forked" aria-label="Fork lucianobragaweb/resume on GitHub">Fork</GitHubButton>
+            <GitHubButton href="https://github.com/lucianobragaweb/resume/issues" data-icon="octicon-issue-opened" aria-label="Issue lucianobragaweb/resume on GitHub">Issue</GitHubButton>
+            <GitHubButton href="https://github.com/lucianobragaweb/resume" data-icon="octicon-star" aria-label="Star lucianobragaweb/resume on GitHub">Star</GitHubButton>
         </footer>
     )
 }
